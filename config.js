1f04e326a80d423ea7627dc30df9179b2f3bf952
BlueKandil <25916618+bluekandil@users.noreply.github.com>
2019-09-26 14:33:33 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 module.exports={  
-    consumer_key:         'rC3dnEWRlcjSbFTHfQiBAmwmw',
-    consumer_secret:      'yAgmBEoMv3YYK1NcpPSwMVVdGJ36OiLoDkV8EsgJ3rSxAps6bT',
-    access_token:         '1176698193813352448-T4059DNIZQoRUM2PoXe8cbCpszhgeb',
-    access_token_secret:  '0ZoJ9TO5NQSMxuehToIjGnAdwYESwJQo1jB662G3Q6Wc0',
+    consumer_key:         ' ',
+    consumer_secret:      '',
+    access_token:         '',
+    access_token_secret:  '',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
     
